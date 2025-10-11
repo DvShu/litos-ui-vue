a22868832906983b709f5cfe7efd3f1dc4ed86db
DvShu <DvShu@users.noreply.github.com>
2025-10-11 09:07:04 +0000

    deploy: 4183240da6ad186cd0ea03fab53a7e5543e51224

---
+++ assets/components_card.md.CHdfuBWk.lean.js
@@ -0,0 +1,62 @@
+import{a as o,_ as n}from"./chunks/theme.DD-_cGzL.js";import{B as u,c as p,o as f,j as d,G as a,at as v,a as l,w as e,k as r}from"./chunks/framework.BSvSrwQ3.js";const m={class:"grid grid-cols-3 gap-4"},P=JSON.parse('{"title":"Card 卡片","description":"","frontmatter":{},"headers":[],"relativePath":"components/card.md","filePath":"components/card.md","lastUpdated":1760171877000}'),x={name:"components/card.md"},C=Object.assign(x,{setup(h){return(b,t)=>{const s=u("CodePreview"),i=u("ClientOnly");return f(),p("div",null,[t[20]||(t[20]=d("h1",{id:"card-卡片",tabindex:"-1"},[l("Card 卡片 "),d("a",{class:"header-anchor",href:"#card-卡片","aria-label":'Permalink to "Card 卡片"'},"​")],-1)),t[21]||(t[21]=d("p",null,"通用卡片应用，常用于后台概览页面。",-1)),t[22]||(t[22]=d("h2",{id:"典型卡片",tabindex:"-1"},[l("典型卡片 "),d("a",{class:"header-anchor",href:"#典型卡片","aria-label":'Permalink to "典型卡片"'},"​")],-1)),t[23]||(t[23]=d("p",null,"包含标题、内容、操作区域。",-1)),a(i,null,{default:e(()=>[a(s,null,{preview:e(()=>[a(r(o),{"header-text":"card title"},{default:e(()=>[...t[0]||(t[0]=[d("div",null,"卡片内容",-1)])]),"header-extra":e(()=>[a(r(n),{text:""},{default:e(()=>[...t[1]||(t[1]=[l("按钮",-1)])]),_:1})]),_:1})]),default:e(()=>[t[2]||(t[2]=d("textarea",{lang:"vue-html"},`<lv-card header-text="card title">
+<template v-slot:default>
+<div>卡片内容</div>
+</template>
+<template v-slot:header-extra>
+<lv-button text>按钮</lv-button>
+</template>
+</lv-card>
+`,-1))]),_:1})]),_:1}),t[24]||(t[24]=d("h2",{id:"简洁卡片",tabindex:"-1"},[l("简洁卡片 "),d("a",{class:"header-anchor",href:"#简洁卡片","aria-label":'Permalink to "简洁卡片"'},"​")],-1)),t[25]||(t[25]=d("p",null,"只包含内容区域",-1)),a(i,null,{default:e(()=>[a(s,null,{preview:e(()=>[a(r(o),{"header-text":"card title","show-header":!1},{default:e(()=>[...t[3]||(t[3]=[d("div",null,"卡片内容",-1)])]),_:1})]),default:e(()=>[t[4]||(t[4]=d("textarea",{lang:"vue-html"},`<lv-card header-text="card title" :show-header="false">
+<template v-slot:default>
+<div>卡片内容</div>
+</template>
+</lv-card>
+`,-1))]),_:1})]),_:1}),t[26]||(t[26]=d("h2",{id:"完整卡片",tabindex:"-1"},[l("完整卡片 "),d("a",{class:"header-anchor",href:"#完整卡片","aria-label":'Permalink to "完整卡片"'},"​")],-1)),t[27]||(t[27]=d("p",null,"卡片包含标题，内容、操作区域以及底部区域。",-1)),a(i,null,{default:e(()=>[a(s,null,{preview:e(()=>[a(r(o),{"header-text":"card title","show-footer":"","footer-text":"底部区域"},{default:e(()=>[...t[5]||(t[5]=[d("div",null,"卡片内容",-1)])]),"header-extra":e(()=>[a(r(n),{text:"",type:"primary"},{default:e(()=>[...t[6]||(t[6]=[l("按钮",-1)])]),_:1})]),_:1})]),default:e(()=>[t[7]||(t[7]=d("textarea",{lang:"vue-html"},`<lv-card header-text="card title" show-footer footer-text="底部区域">
+<template v-slot:default>
+<div>卡片内容</div>
+</template>
+<template v-slot:header-extra>
+<lv-button text>按钮</lv-button>
+</template>
+</lv-card>
+`,-1))]),_:1})]),_:1}),t[28]||(t[28]=v("",2)),a(i,null,{default:e(()=>[a(s,null,{preview:e(()=>[a(r(o),{"show-footer":!0},{header:e(()=>[...t[8]||(t[8]=[d("span",{style:{color:"red"}},"卡片标题",-1)])]),default:e(()=>[...t[9]||(t[9]=[d("div",null,"卡片内容",-1)])]),"header-extra":e(()=>[a(r(n),{text:""},{default:e(()=>[...t[10]||(t[10]=[l("按钮",-1)])]),_:1})]),footer:e(()=>[...t[11]||(t[11]=[d("span",{style:{color:"orange"}},"卡片标题",-1)])]),_:1})]),default:e(()=>[t[12]||(t[12]=d("textarea",{lang:"vue-html"},`<lv-card :show-footer="true">
+<template v-slot:header>
+<span style="color: red">卡片标题</span>
+</template>
+<template v-slot:default>
+<div>卡片内容</div>
+</template>
+<template v-slot:header-extra>
+<lv-button text>按钮</lv-button>
+</template>
+<template v-slot:footer>
+<span style="color: orange">卡片标题</span>
+</template>
+</lv-card>
+`,-1))]),_:1})]),_:1}),t[29]||(t[29]=d("h2",{id:"栅格卡片",tabindex:"-1"},[l("栅格卡片 "),d("a",{class:"header-anchor",href:"#栅格卡片","aria-label":'Permalink to "栅格卡片"'},"​")],-1)),t[30]||(t[30]=d("p",null,"在系统概览页面常常和栅格进行配合。通常用于管理后台的工作台聚合展示。",-1)),t[31]||(t[31]=d("p",null,[l("关于栅格布局，框架不提供，推荐使用 "),d("a",{href:"https://unocss.dev/interactive/?s=grid",target:"_blank",rel:"noreferrer"},"unocss"),l(" 或者 "),d("a",{href:"https://www.tailwindcss.cn/docs/display#grid",target:"_blank",rel:"noreferrer"},"tailwindcss"),l(" 的 "),d("code",null,"grid"),l(" 实现")],-1)),a(i,null,{default:e(()=>[a(s,null,{preview:e(()=>[d("div",m,[a(r(o),{"header-text":"卡片标题1"},{default:e(()=>[...t[13]||(t[13]=[d("div",null,"卡片内容1",-1)])]),"header-extra":e(()=>[a(r(n),{text:""},{default:e(()=>[...t[14]||(t[14]=[l("按钮",-1)])]),_:1})]),_:1}),a(r(o),{"header-text":"卡片标题2"},{default:e(()=>[...t[15]||(t[15]=[d("div",null,"卡片内容2",-1)])]),"header-extra":e(()=>[a(r(n),{text:""},{default:e(()=>[...t[16]||(t[16]=[l("按钮",-1)])]),_:1})]),_:1}),a(r(o),{"header-text":"卡片标题3"},{default:e(()=>[...t[17]||(t[17]=[d("div",null,"卡片内容3",-1)])]),"header-extra":e(()=>[a(r(n),{text:""},{default:e(()=>[...t[18]||(t[18]=[l("按钮",-1)])]),_:1})]),_:1})])]),default:e(()=>[t[19]||(t[19]=d("textarea",{lang:"vue-html"},`  <div class="grid grid-cols-3 gap-4">
+    <lv-card header-text="卡片标题1">
+      <template v-slot:default>
+        <div>卡片内容1</div>
+      </template>
+      <template v-slot:header-extra>
+        <lv-button text>按钮</lv-button>
+      </template>
+    </lv-card>
+    <lv-card header-text="卡片标题2">
+      <template v-slot:default>
+        <div>卡片内容2</div>
+      </template>
+      <template v-slot:header-extra>
+        <lv-button text>按钮</lv-button>
+      </template>
+    </lv-card>
+    <lv-card header-text="卡片标题3">
+      <template v-slot:default>
+        <div>卡片内容3</div>
+      </template>
+      <template v-slot:header-extra>
+        <lv-button text>按钮</lv-button>
+      </template>
+    </lv-card>
+  </div>
+  `,-1))]),_:1})]),_:1}),t[32]||(t[32]=v("",5))])}}});export{P as __pageData,C as default};
---
+++ assets/components_popover.md.B2abBrMZ.lean.js
@@ -0,0 +1,101 @@
+import{G as d,_ as p}from"./chunks/theme.DD-_cGzL.js";import{p as m,B as s,c,o as f,at as u,G as o,j as n,w as e,k as l,a as r}from"./chunks/framework.BSvSrwQ3.js";const b={class:"popover-p-row"},h={class:"popover-p-row"},P={class:"popover-p-row"},w={class:"popover-p-row"},q=JSON.parse('{"title":"Popover 弹出气泡","description":"","frontmatter":{},"headers":[],"relativePath":"components/popover.md","filePath":"components/popover.md","lastUpdated":1760171877000}'),_={name:"components/popover.md"},E=Object.assign(_,{setup(x){const a=m();function g(){a.value!=null&&(a.value.isShow()?a.value.hide():a.value.show("#popoverTo"))}return(T,t)=>{const i=s("CodePreview"),v=s("ClientOnly");return f(),c("div",null,[t[23]||(t[23]=u("",5)),o(v,null,{default:e(()=>[o(i,null,{preview:e(()=>[o(l(d),{content:"悬浮提示"},{trigger:e(()=>[...t[0]||(t[0]=[n("span",null,"悬浮",-1)])]),_:1})]),default:e(()=>[t[1]||(t[1]=n("textarea",{lang:"vue-html"},`  <lv-popover content="悬浮提示">
+    <template #trigger>
+      <span>悬浮</span>
+    </template>
+  </lv-popover>
+  `,-1))]),_:1})]),_:1}),t[24]||(t[24]=n("h3",{id:"触发方式",tabindex:"-1"},[r("触发方式 "),n("a",{class:"header-anchor",href:"#触发方式","aria-label":'Permalink to "触发方式"'},"​")],-1)),t[25]||(t[25]=n("p",null,[r("两种触发方式：鼠标移入["),n("code",null,"hover"),r("]、点击["),n("code",null,"click"),r("]；通过 "),n("code",null,"trigger"),r(" 属性设置触发方式。")],-1)),o(v,null,{default:e(()=>[o(i,null,{preview:e(()=>[o(l(d),{content:"悬浮提示",trigger:"hover"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[2]||(t[2]=[r("悬浮",-1)])]),_:1})]),_:1}),o(l(d),{content:"点击提示",trigger:"click"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[3]||(t[3]=[r("点击",-1)])]),_:1})]),_:1})]),default:e(()=>[t[4]||(t[4]=n("textarea",{lang:"vue-html"},`  <lv-popover content="悬浮提示" trigger='hover'>
+    <template #trigger>
+      <lv-button>悬浮</lv-button>
+    </template>
+  </lv-popover>
+  <lv-popover content="点击提示" trigger='click'>
+    <template #trigger>
+      <lv-button>点击</lv-button>
+    </template>
+  </lv-popover>
+  `,-1))]),_:1})]),_:1}),t[26]||(t[26]=n("h3",{id:"位置",tabindex:"-1"},[r("位置 "),n("a",{class:"header-anchor",href:"#位置","aria-label":'Permalink to "位置"'},"​")],-1)),t[27]||(t[27]=n("p",null,[r("有 "),n("code",null,"12"),r(" 个弹出位置。通过 "),n("code",null,"placement"),r(" 属性设置弹出位置。")],-1)),o(v,null,{default:e(()=>[o(i,null,{preview:e(()=>[n("div",b,[o(l(d),{content:"提示内容",placement:"top-start"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[5]||(t[5]=[r("top-start",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"top"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[6]||(t[6]=[r("top",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"top-end"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[7]||(t[7]=[r("top-end",-1)])]),_:1})]),_:1})]),n("div",h,[o(l(d),{content:"提示内容",placement:"left-start"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[8]||(t[8]=[r("left-start",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"left"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[9]||(t[9]=[r("left",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"left-end"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[10]||(t[10]=[r("left-end",-1)])]),_:1})]),_:1})]),n("div",P,[o(l(d),{content:"提示内容",placement:"right-start"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[11]||(t[11]=[r("right-start",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"right"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[12]||(t[12]=[r("right",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"right-end"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[13]||(t[13]=[r("right-end",-1)])]),_:1})]),_:1})]),n("div",w,[o(l(d),{content:"提示内容",placement:"bottom-start"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[14]||(t[14]=[r("bottom-start",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"bottom"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[15]||(t[15]=[r("bottom",-1)])]),_:1})]),_:1}),o(l(d),{content:"提示内容",placement:"bottom-end"},{trigger:e(()=>[o(l(p),null,{default:e(()=>[...t[16]||(t[16]=[r("bottom-end",-1)])]),_:1})]),_:1})])]),default:e(()=>[t[17]||(t[17]=n("textarea",{lang:"vue-html"},`  <div class="popover-p-row">
+    <lv-popover content="提示内容" placement="top-start">
+      <template #trigger>
+        <lv-button>top-start</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="top">
+      <template #trigger>
+        <lv-button>top</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="top-end">
+      <template #trigger>
+        <lv-button>top-end</lv-button>
+      </template>
+    </lv-popover>
+  </div>
+  <div class="popover-p-row">
+    <lv-popover content="提示内容" placement="left-start">
+      <template #trigger>
+        <lv-button>left-start</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="left">
+      <template #trigger>
+        <lv-button>left</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="left-end">
+      <template #trigger>
+        <lv-button>left-end</lv-button>
+      </template>
+    </lv-popover>
+  </div>
+  <div class="popover-p-row">
+    <lv-popover content="提示内容" placement="right-start">
+      <template #trigger>
+        <lv-button>right-start</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="right">
+      <template #trigger>
+        <lv-button>right</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="right-end">
+      <template #trigger>
+        <lv-button>right-end</lv-button>
+      </template>
+    </lv-popover>
+  </div>
+  <div class="popover-p-row">
+    <lv-popover content="提示内容" placement="bottom-start">
+      <template #trigger>
+        <lv-button>bottom-start</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="bottom">
+      <template #trigger>
+        <lv-button>bottom</lv-button>
+      </template>
+    </lv-popover>
+    <lv-popover content="提示内容" placement="bottom-end">
+      <template #trigger>
+        <lv-button>bottom-end</lv-button>
+      </template>
+    </lv-popover>
+  </div>
+  `,-1))]),_:1})]),_:1}),t[28]||(t[28]=n("h3",{id:"非受控模式",tabindex:"-1"},[r("非受控模式 "),n("a",{class:"header-anchor",href:"#非受控模式","aria-label":'Permalink to "非受控模式"'},"​")],-1)),t[29]||(t[29]=n("p",null,[r("不需要自动弹出，需要进行手动控制是否展示；通过传递 "),n("code",null,"visible"),r(" 属性设置是否展示，然后设置 "),n("code",null,"to"),r(" 属性指向弹层对标节点。")],-1)),o(v,null,{default:e(()=>[o(i,null,{preview:e(()=>[t[19]||(t[19]=n("span",{id:"popoverTo"},"对标元素",-1)),o(l(p),{onClick:g,class:"ml-10"},{default:e(()=>[...t[18]||(t[18]=[r("弹出/隐藏",-1)])]),_:1}),o(l(d),{ref_key:"$popover",ref:a,content:"提示内容",trigger:"manual"},null,512)]),default:e(()=>[t[20]||(t[20]=n("textarea",{lang:"vue-html"},`  <script setup>
+  import { ref } from 'vue'
+  const $popover = ref()
+  function toggleShow() {
+    $popover.value.toggle();
+  }
+  <\/script>
+  <template>
+    <span id="popoverTo">对标元素</span>
+    <lv-button @click='toggleShow' class="ml-10">弹出/隐藏</lv-button>
+    <lv-popover reference="#popoverTo" ref="$popover" content="提示内容" trigger="manual"></lv-popover>
+  </template>
+  `,-1))]),_:1})]),_:1}),t[30]||(t[30]=n("h3",{id:"不显示箭头",tabindex:"-1"},[r("不显示箭头 "),n("a",{class:"header-anchor",href:"#不显示箭头","aria-label":'Permalink to "不显示箭头"'},"​")],-1)),t[31]||(t[31]=n("p",null,[r("通过传递 "),n("code",null,"show-arrow"),r(" 为 "),n("code",null,"false"),r(" 来取消箭头显示")],-1)),o(v,null,{default:e(()=>[o(i,null,{preview:e(()=>[o(l(d),{content:"悬浮提示","show-arrow":!1},{trigger:e(()=>[...t[21]||(t[21]=[n("span",null,"悬浮",-1)])]),_:1})]),default:e(()=>[t[22]||(t[22]=n("textarea",{lang:"vue-html"},`  <lv-popover content="悬浮提示" :show-arrow="false">
+    <template #trigger>
+      <span>悬浮</span>
+    </template>
+  </lv-popover>
+  `,-1))]),_:1})]),_:1}),t[32]||(t[32]=u("",7))])}}});export{q as __pageData,E as default};
