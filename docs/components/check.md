816013730f23d7f7fe0183fd1177de712200f1b9
Tenny <joel.shu@qq.com>
2025-10-20 17:30:11 +0800

    feat: Check

--- docs/components/check.md
+++ docs/components/check.md
@@ -5,7 +5,10 @@
 ## 演示
 
 <script setup>
-  import { Check, Checkbox } from "../../src"
+  import { ref } from 'vue';
+  import { Check, Checkbox } from "../../src";
+
+  const checked1 = ref(false);
 </script>
 
 ### 使用
@@ -22,7 +25,8 @@
   </template>
   </textarea>
   <template #preview>
-    <Check label="选项"></Check>
+    <Check label="选项" v-model="checked1" type="checkbox"></Check>
+    <hr />
   </template>
   </CodePreview>
 </ClientOnly>
@@ -35,3 +39,11 @@
 | 参数 | 说明 | 类型 | 默认值 |
 | --- | --- | --- | --- |
 | x | x | x | x |
+
+### 样式变量
+
+<!-- prettier-ignore -->
+| 名称 | 描述 | 默认值 |
+| --- | --- | --- |
+| `--l-check-size` | 选择框的大小 | `16px` |
+| `--l-check-font-size` | 选择框的文字大小 | `var(--l-check-size)` |
--- src/components/check/Check.vue
+++ src/components/check/Check.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, h } from 'vue';
+import { defineComponent, h, computed } from 'vue';
 import type { PropType } from 'vue';
 
 export default defineComponent({
@@ -8,14 +8,76 @@ export default defineComponent({
       type: String as PropType<'radio' | 'checkbox'>,
       default: 'radio',
     },
+    checked: {
+      type: Boolean,
+      default: undefined,
+    },
+    /** 中间状态 */
+    indeterminate: {
+      type: Boolean,
+      default: false,
+    },
+    name: String,
+    value: {
+      type: [String, Boolean, Number],
+      default: undefined,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    modelValue: {
+      type: [Boolean, String, Number],
+      default: false,
+    },
   },
-  setup(props) {
+  emits: ['change', 'update:modelValue'],
+  setup(props, { emit }) {
+    function handleChange(e: Event) {
+      const target = e.target as HTMLInputElement;
+      const checked = target.checked;
+      const value = props.value == null ? checked : props.value;
+      emit('update:modelValue', value);
+      emit('change', value);
+    }
+
+    const isCheck = computed(() => {
+      console.log(props.modelValue);
+      if (props.value != null) {
+        return props.modelValue === props.value;
+      }
+      if (props.checked != null) {
+        return props.checked;
+      }
+      return props.modelValue as boolean;
+    });
+
     return () =>
-      h('label', { class: 'l-check' }, [
-        h('div', 'D'),
-        h('div', 'H'),
-        h('input', { type: props.type, class: 'l-check__inner' }),
-      ]);
+      h(
+        'label',
+        {
+          class: [
+            'l-check',
+            `l-check--${props.type}`,
+            isCheck.value ? 'l-check--checked' : '',
+          ],
+        },
+        [
+          h('span', { class: 'l-check--input' }, [
+            h('input', {
+              type: props.type,
+              class: 'l-check--original',
+              checked: props.checked,
+              name: props.name,
+              value: props.value,
+              disabled: props.disabled,
+              onChange: handleChange,
+            }),
+            h('span', { class: 'l-check--inner' }),
+          ]),
+          h('span', '选择协议'),
+        ],
+      );
   },
 });
 </script>
