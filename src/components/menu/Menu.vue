58dff4d688009e367447cf9a93f0172d1d93b591
Tenny <joel.shu@qq.com>
2025-12-11 01:17:41 +0800

    feat: Menu

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
-import { defineComponent, VNode, PropType, h } from 'vue';
+import { defineComponent, h } from 'vue';
+import type { VNode, PropType } from 'vue';
 
 type MenuItemType = {
   label: string;
@@ -18,9 +19,44 @@ export default defineComponent({
     },
   },
   setup(props) {
-    console.log(props);
+    function renderMenuItems(items: MenuItemType[]) {
+      console.log(items);
+      return items.map((item) => {
+        const hasChildren = item.children && item.children.length > 0;
+        const $container: VNode[] = [];
+        if (hasChildren) {
+          $container.push(
+            h(
+              'div',
+              { class: 'l-menu-submenu-title', role: 'menuitem' },
+              item.label,
+            ),
+          );
+          $container.push(
+            h(
+              'ul',
+              { class: 'l-menu l-menu-sub' },
+              renderMenuItems(item.children as MenuItemType[]),
+            ),
+          );
+        }
+        return h(
+          'li',
+          {
+            class: `l-menu-${hasChildren ? 'submenu' : 'item'}`,
+            role: hasChildren ? 'submenu' : 'menuitem',
+          },
+          [item.icon, item.label],
+        );
+      });
+    }
+
     return () => {
-      return h('div', { class: 'l-menu' }, 'Menu');
+      return h(
+        'ul',
+        { class: 'l-menu', role: 'menu' },
+        renderMenuItems(props.items),
+      );
     };
   },
 });
